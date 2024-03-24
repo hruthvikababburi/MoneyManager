6ee30e0f4bf8cd7e54e6e490377b9b7d99bf028c
hruthvika <hruthvikababburi@gmail.com>
2024-03-24 05:13:18 +0000

    Pushing code

---
+++ src/components/MoneyDetails/index.js
@@ -0,0 +1,57 @@
+// Write your code here
+import './index.css'
+
+const MoneyDetails = props => {
+  const {incomeAmount, expensesAmount} = props
+
+  return (
+    <div className="money-details-container">
+      <div className="money-details-item-container balance-amount-container">
+        <img
+          className="money-details-img"
+          src="https://assets.ccbp.in/frontend/react-js/money-manager/balance-image.png"
+          alt="balance"
+        />
+        <div className="money-details-data-container">
+          <p className="money-detail-text">Your Balance</p>
+          {/* <p data-testid="balanceAmount" className="amount-text"> */}
+          <p data-testid="balanceAmount" className="amount-text">
+            Rs {incomeAmount - expensesAmount}
+          </p>
+        </div>
+      </div>
+
+      <div className="money-details-item-container income-amount-container">
+        <img
+          className="money-details-img"
+          src="https://assets.ccbp.in/frontend/react-js/money-manager/income-image.png"
+          alt="income"
+        />
+        <div className="money-details-data-container">
+          <p className="money-detail-text">Your Income</p>
+          {/* <p data-testid="incomeAmount" className="amount-text"> */}
+          <p data-testid="incomeAmount" className="amount-text">
+            Rs {incomeAmount}
+          </p>
+        </div>
+      </div>
+
+      <div className="money-details-item-container expenses-amount-container">
+        <img
+          className="money-details-img"
+          src="https://assets.ccbp.in/frontend/react-js/money-manager/expenses-image.png"
+          alt="expenses"
+        />
+        <div className="money-details-data-container">
+          <p className="money-detail-text">Your Expenses</p>
+          {/* <p data-testid="expensesAmount" className="amount-text"> */}
+          <p data-testid="expensesAmount" className="amount-text">
+            Rs {expensesAmount}
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MoneyDetails
